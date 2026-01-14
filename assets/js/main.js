adad8f919f40d765f3979125743f7ad3c914466e
elmkalizineb <elmkalizineb@gmail.com>
2026-01-14 19:44:14 +0100

    Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,36 @@
 
 /*==================== MENU SHOW & HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click',() =>{
+        navMenu.classList.add('show-menu');
+    });
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click',() =>{
+        navMenu.classList.remove('show-menu');
+    });
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink=document.querySelectorAll('.nav-link');
+
+const linkAction = ()=>{
+    const navMenu = document.getElementById('nav-menu');
+
+    navMenu.classList.remove('show-menu');
+};
+navLink.forEach((link)=>{
+    link.addEventListener('click',linkAction);
+});
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 const scrolHeader = () => {
@@ -61,21 +84,21 @@ window.addEventListener('DOMContentLoaded', () => {
     const toggleBtn = document.getElementById("theme-toggle");
 
     function applyTheme(theme) {
-        if (theme == 'light' ){
+        if (theme == 'light') {
             document.body.classList.add('light-theme');
             toggleBtn.classList.remove('ri-sun-line');
             toggleBtn.classList.add('ri-moon-line');
-        }else{
+        } else {
             // console.log('dark');
             document.body.classList.remove('light-theme');
             toggleBtn.classList.add('ri-sun-line');
             toggleBtn.classList.remove('ri-moon-line');
         }
 
-        localStorage.setItem('theme',theme);
+        localStorage.setItem('theme', theme);
     }
 
-    const savedTheme=localStorage.getItem('theme') || 'dark';
+    const savedTheme = localStorage.getItem('theme') || 'dark';
     applyTheme(savedTheme);
 
     toggleBtn.addEventListener('click', () => {
@@ -84,81 +107,81 @@ window.addEventListener('DOMContentLoaded', () => {
 
     });
 });
-    /*==================== MIXITUP FILTER PORTFOLIO ====================*/
-    var mixer = mixitup('.work-container', {
-        selectors: {
-            target: '.mix'
-        },
-        animation: {
-            duration: 300
-        }
+/*==================== MIXITUP FILTER PORTFOLIO ====================*/
+var mixer = mixitup('.work-container', {
+    selectors: {
+        target: '.mix'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/* Active work */
+const linkWork = document.querySelectorAll(".work-item");
+
+function activeWork() {
+    linkWork.forEach((a) => {
+        a.classList.remove('active-work');
     });
 
-    /* Active work */
-    const linkWork = document.querySelectorAll(".work-item");
+    this.classList.add('active-work');
+}
+linkWork.forEach((a) => a.addEventListener('click', activeWork));
 
-    function activeWork() {
-        linkWork.forEach((a) => {
-            a.classList.remove('active-work');
-        });
+/*==================== EMAIL JS ====================*/
+const contactForm = document.getElementById('contact-form'),
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactMessage = document.getElementById('contact-message'),
+    message = document.getElementById('message');
 
-        this.classList.add('active-work');
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (contactName.value === ''
+        || contactEmail === ''
+        || contactMessage === '') {
+        message.textContent = 'Write all the input feilds ';
+
+        setTimeout(() => {
+            message.textContent = '';
+        }, 3000);
+    } else {
+        emailjs.sendForm('service_id',
+            'template_id',
+            '#contact-form').then(
+                () => {
+                    message.textContent = 'Message sent  👌 ';
+
+                    setTimeout(() => {
+                        message.textContent = '';
+                    }, 5000);
+                },
+                (error) => {
+                    alert('OOPs ! Something went Wrong ... ', error);
+                },
+            );
+        contactName.value = '';
+        contactEmail.value = '';
+        contactMessage.value = '';
+
     }
-    linkWork.forEach((a) => a.addEventListener('click', activeWork));
+};
 
-    /*==================== EMAIL JS ====================*/
-    const contactForm = document.getElementById('contact-form'),
-        contactName = document.getElementById('contact-name'),
-        contactEmail = document.getElementById('contact-email'),
-        contactMessage = document.getElementById('contact-message'),
-        message = document.getElementById('message');
-
-    const sendEmail = (e) => {
-        e.preventDefault();
-
-        if (contactName.value === ''
-            || contactEmail === ''
-            || contactMessage === '') {
-            message.textContent = 'Write all the input feilds ';
-
-            setTimeout(() => {
-                message.textContent = '';
-            }, 3000);
-        } else {
-            emailjs.sendForm('service_id',
-                'template_id',
-                '#contact-form').then(
-                    () => {
-                        message.textContent = 'Message sent  👌 ';
-
-                        setTimeout(() => {
-                            message.textContent = '';
-                        }, 5000);
-                    },
-                    (error) => {
-                        alert('OOPs ! Something went Wrong ... ', error);
-                    },
-                );
-            contactName.value = '';
-            contactEmail.value = '';
-            contactMessage.value = '';
-
-        }
-    };
-
-    contactForm.addEventListener('submit', sendEmail);
+contactForm.addEventListener('submit', sendEmail);
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
-    origin:'top',
-    distance:'60px',
-    duration:2500,
-    delay:400,
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
 });
 
 sr.reveal(`.home-data`);
-sr.reveal(`.home-img-wrapper`,{delay:500});
-sr.reveal(`.home-social`,{delay:600}); 
-sr.reveal(`.services-card ,.mix`,{interval:100}); 
-sr.reveal(`.skills-developer ,.resume-left , .contact-group`,{origin:'left'});
-sr.reveal(`.skills-designer , .resume-right ,.contact-form`,{origin:'right'});
+sr.reveal(`.home-img-wrapper`, { delay: 500 });
+sr.reveal(`.home-social`, { delay: 600 });
+sr.reveal(`.services-card ,.mix`, { interval: 100 });
+sr.reveal(`.skills-developer ,.resume-left , .contact-group`, { origin: 'left' });
+sr.reveal(`.skills-designer , .resume-right ,.contact-form`, { origin: 'right' });
